7ed0a51088f2059444caa315823eb73ffc48c276
Chris Braithwaite <braithwaitec@msoe.edu>
2017-10-07 12:08:15 -0500

    Have a fragment loading into an activity

--- app/src/main/java/msoe/supermileage/SetupActivity.java
+++ app/src/main/java/msoe/supermileage/SetupActivity.java
@@ -1,13 +1,44 @@
 package msoe.supermileage;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SetupActivity extends AppCompatActivity {
+public class SetupActivity
+        extends AppCompatActivity
+        implements SelectServerFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+
+        if (findViewById(R.id.fragment_container) == null) {
+            // shouldn't happen
+        } else {
+            if (savedInstanceState == null) {
+                // create a fragment and load it to container
+                SelectServerFragment fragment = new SelectServerFragment();
+
+                // because this was started from an intent
+                fragment.setArguments(getIntent().getExtras());
+
+                // add home to the container
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .add(
+                                R.id.fragment_container,
+                                fragment
+                        )
+                        .commit();
+            } else {
+                // being restored from previous state
+            }
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        // ignored
     }
 }
