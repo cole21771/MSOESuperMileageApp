2f55dc6ae4ba151779ed8283ccd5907672ff0525
Chris Braithwaite <braithwaitec@msoe.edu>
2017-10-30 11:29:33 -0500

    add interface for collection activity

--- app/src/main/java/msoe/supermileage/activities/CollectionActivity.java
+++ app/src/main/java/msoe/supermileage/activities/CollectionActivity.java
@@ -1,8 +1,11 @@
 package msoe.supermileage.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import msoe.supermileage.App;
 import msoe.supermileage.R;
@@ -15,6 +18,9 @@ public class CollectionActivity extends AppCompatActivity {
     private String serverName;
     private String serverIP;
     private String config;
+    private ImageView serverAvailabilityImageView;
+    private TextView statusTextView;
+    private ToggleButton startStopToggleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +30,7 @@ public class CollectionActivity extends AppCompatActivity {
         this.app = (App) getApplication();
         this.app.setActivity(this);
 
-        this.toolbar = findViewById(R.id.setup_toolbar);
+        this.toolbar = findViewById(R.id.collection_toolbar);
         setSupportActionBar(this.toolbar);
         this.toolbar.setTitle("Super Mileage App");
 
@@ -32,6 +38,14 @@ public class CollectionActivity extends AppCompatActivity {
         this.serverIP = getIntent().getStringExtra(App.EXTRA_SM_SERVER_IP);
         this.config = getIntent().getStringExtra(App.EXTRA_SM_CONFIG);
 
-        
+        this.serverAvailabilityImageView = findViewById(R.id.server_available_imageview);
+
+        TextView serverNameTextview = findViewById(R.id.server_name_textview);
+        serverNameTextview.setText(this.serverName);
+
+        this.statusTextView = findViewById(R.id.status_textview);
+
+        this.startStopToggleButton = findViewById(R.id.data_send_togglebutton);
+
     }
 }
